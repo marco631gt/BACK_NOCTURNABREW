1ea55c637789e5e284f250fdf68edd1767957626
Emmanuel <st4004@utr.edu.mx>
2025-10-27 10:49:52 -0600

    creation of schemas for Mongo

---
+++ MODELS/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, {version} from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    nombre:{
+        type: String,
+        require: true,
+        lowercase: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+
+    },
+    password:{
+        type: String,
+        require: true,
+    },
+    rol:{
+        type: String,
+        require: false,
+    }
+}) 
+
+export const User = mongoose.model('User', userSchema)
